ff926fdec8c2f543a868738c685a4bb7924f74e0
Sumit Saxena <engineersumitsaxena@gmail.com>
2025-06-25 12:57:55 +0530

    Fix: removed missing whoModel import

--- src/Components/MiniNav.jsx
+++ src/Components/MiniNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Destination from './Destination'
-import WhoModel from './whoModel'
+import WhoModel from './WhoModel'
 import DateSelectorModal from './DateModal'
 function MiniNav() {
   return (
